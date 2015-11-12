ed093a7baa329bfb9c051ce1bf5542b12fa2641e
John Fresco <john.fresco@utah.edu>
2015-11-12 11:30:34 -0700

    Animate previewer

--- src/previewwidget.cpp
+++ src/previewwidget.cpp
@@ -7,16 +7,37 @@ PreviewWidget::PreviewWidget(QWidget *parent)
     , layout(new QVBoxLayout)
     , frame(0)
     , scale(1)
+    , timer(new QTimer)
 {
     view->setScene(scene);
     layout->addWidget(view);
     this->setLayout(layout);
+
+    connect(timer, &QTimer::timeout, this, &PreviewWidget::nextFrame);
+    timer->start(66);
+
 }
 
 void PreviewWidget::changeBuffer(std::shared_ptr<Buffer> buffer) {
+
     this->buffer = buffer;
 
     scene->clear();
     scene->addPixmap(QPixmap::fromImage(
                          *(this->buffer->fetchSnapshot().front().get())));
+
+}
+
+
+void PreviewWidget::nextFrame() {
+    auto frames = this->buffer->fetchSnapshot();
+    qDebug() << frame << "\n" << frames.size();
+    if (frame >= (int) frames.size()) {
+        frame = 0;
+    }
+
+    scene->clear();
+    scene->addPixmap(QPixmap::fromImage(*(frames.at(frame).get())));
+
+    frame++;
 }
